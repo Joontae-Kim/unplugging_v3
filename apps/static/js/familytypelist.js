6e4cd8f1e9fe13d295f3ead0e968444af6f7af69
bexellenting@gmail.com <bexellenting@gmail.com>
2017-02-08 16:12:39 +0900

    Grouping appliance group by array

---
+++ apps/static/js/familytypelist.js
@@ -0,0 +1,192 @@
+  var family_type12 = [{
+          "Id": "item1",
+          "name": "냉장고"},
+        {
+          "Id": "item4",
+          "name": "전기밥솥"},
+        {
+          "Id": "item10",
+          "name": "전자레인지"},
+        {
+          "Id": "item13",
+          "name": "TV"},
+        {
+          "Id": "item17",
+          "name": "세탁기"},
+        {
+          "Id": "item14",
+          "name": "형광등"},
+        {
+          "Id": "item21",
+          "name": "충전기"},
+        {
+          "Id": "item22",
+          "name": "노트북"},
+        {
+          "Id": "item23",
+          "name": "IPTV"},
+        {
+          "Id": "item24",
+          "name": "드라이기"},
+        {
+          "Id": "item26",
+          "name": "선풍기"},
+        {
+          "Id": "item25",
+          "name": "에어컨"},
+        {
+          "Id": "item27",
+          "name": "세탁기"},
+        {
+          "Id": "item29",
+          "name": "온수매트"},
+        {
+          "Id": "item11",
+          "name": "비데"}
+    ] // family_type1 
+
+  var family_type3 = [{
+          "Id": "item1",
+          "name": "냉장고"},
+        {
+          "Id": "item4",
+          "name": "전기밥솥"},
+        {
+          "Id": "item10",
+          "name": "전자레인지"},
+        {
+          "Id": "item13",
+          "name": "TV"},
+        {
+          "Id": "item17",
+          "name": "세탁기"},
+        {
+          "Id": "item14",
+          "name": "형광등"},
+        {
+          "Id": "item21",
+          "name": "충전기"},
+        {
+          "Id": "item22",
+          "name": "노트북"},
+        {
+          "Id": "item23",
+          "name": "IPTV"},
+        {
+          "Id": "item24",
+          "name": "드라이기"},
+        {
+          "Id": "item26",
+          "name": "선풍기"},
+        {
+          "Id": "item25",
+          "name": "에어컨"},
+        {
+          "Id": "item27",
+          "name": "온풍기"},
+        {
+          "Id": "item29",
+          "name": "온수매트"},
+        {
+          "Id": "item5",
+          "name": "김치냉장고"},
+        {
+          "Id": "item11",
+          "name": "비데"},
+        {
+          "Id": "item18",
+          "name": "컴퓨터"},
+        {
+          "Id": "item19",
+          "name": "모니터"}
+    ] // family_type3 
+
+  var appname_list = [{
+          "Id": "item1",
+          "name": "냉장고",
+          "unit": "용량(L)"},
+        {
+          "Id": "item4",
+          "name": "전기밥솥",
+          "unit": "용량(인용)"},
+        {
+          "Id": "item5",
+          "name": "김치냉장고",
+          "unit": "용량(L)"},
+        {
+          "Id": "item10",
+          "name": "전자레인지",
+          "unit": "용량(L)"},
+        {
+          "Id": "item11",
+          "name": "비데",
+          "unit": "종류"},
+        {
+          "Id": "item13",
+          "name": "TV",
+          "unit": "인치"},
+        {
+          "Id": "item14",
+          "name": "형광등",
+          "unit": "종류"},
+        {
+          "Id": "item16",
+          "name": "다리미",
+          "unit": "종류"},
+        {
+          "Id": "item17",
+          "name": "세탁기",
+          "unit": "용량(kg)"},
+        {
+          "Id": "item18",
+          "name": "컴퓨터",
+          "unit": "본체 종류"},
+        {
+          "Id": "item19",
+          "name": "모니터",
+          "unit": "인치(이상)"},
+        {
+          "Id": "item21",
+          "name": "충전기",
+          "unit": "종류"},
+        {
+          "Id": "item22",
+          "name": "노트북",
+          "unit": "제조사, 종류"},
+        {
+          "Id": "item23",
+          "name": "IPTV",
+          "unit": "종류"},
+        {
+          "Id": "item24",
+          "name": "드라이기",
+          "unit": "크기"},
+        {
+          "Id": "item26",
+          "name": "선풍기",
+          "unit": "종류"},
+        {
+          "Id": "item25",
+          "name": "에어컨",
+          "unit": "종류 / 거실 평수 이상"},
+        {
+          "Id": "item27",
+          "name": "온풍기",
+          "unit": "종류"},
+        {
+          "Id": "item28",
+          "name": "전열난로",
+          "unit": "종류"},
+        {
+          "Id": "item29",
+          "name": "온수매트",
+          "unit": "사용 인원"},
+        {
+          "Id": "item30",
+          "name": "전기장판",
+          "unit": "사용 인원"},
+        {
+          "Id": "item31",
+          "name": "드럼세탁기",
+          "unit": "용량(L)"}
+    ]
